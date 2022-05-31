e0d9e3caa0653bd07f3e7c11ce63dd639976107b
RevvAmit <amit.k@revvsales.com>
2022-05-31 21:41:18 +0530

    feat: added simpro test cases

---
+++ cypress/pages/order_books_page.js
@@ -0,0 +1,16 @@
+export const ORDER_BOOK_PAGE = {
+   FICTION_RADIO_BUTTON:'[value="Fiction"]',
+   DRAMA_RADIO_BUTTON:'[value="Drama"]',
+   CHOOSE_BOOK_OPTION:'.bookoptions',
+   UNITS_INPUT:'[name="units"]',
+   PRICE_INPUT:'[name="price"]',
+   SUBMIT_BUTTON:'[name="submit"]',
+   DISCOUNT_CHECKBOX:'[name="discount"]',
+   DISCOUNT_INPUT:'.discountvalue',
+
+   // order details
+   TRANSACTION_RECORD_TABLE_COLUMN:'[id="transactionsection"]>th',
+   TRANSACTION_RECORD_TABLE_ROW:'[id="transactionsection"]>tr>td',
+   DELETE_TRANSACTION_RECORD_TABLE_ROW_BUTTON:'.removeRecord'
+
+}
